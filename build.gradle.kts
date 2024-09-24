990bf2e00261fc78612ddd3c9748de241493d15a
Happy <admin@maoyuan.us>
2024-09-24 22:15:11 +0800

    fuck build

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,15 +22,6 @@ plugins {
     alias(libs.plugins.runPaper)
 }
 
-
-tasks.withType<Javadoc> {
-    options.encoding = "UTF-8" // 设置编码
-
-    // 使用额外的参数禁用 Javadoc 检查
-    options.additionalParameter = "-Xdoclint:none" // 禁用所有检查
-    // 如果需要，还可以设置其他 Javadoc 选项
-    options.isFailOnError = false // 这停止因为错误导致的构建失败
-}
 val gitCommitId: String by lazy {
     // 执行 Git 命令获取短 SHA ID
     "git rev-parse --short HEAD".run {
